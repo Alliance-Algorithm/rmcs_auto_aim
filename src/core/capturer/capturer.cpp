8072e09d683561eba390066455b3424bf3d7b776
7emotions <paradise_c@qq.com>
2024-10-24 04:47:55 +0000

    Feat: Add video mode for debug

--- src/core/capturer/capturer.cpp
+++ src/core/capturer/capturer.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <memory>
+#include <opencv2/videoio.hpp>
 #include <thread>
 
 #include <opencv2/core/mat.hpp>
@@ -21,13 +22,28 @@ public:
         register_input("/predefined/update_count", update_count_);
         register_output("/auto_aim/camera", img_);
 
-        hikcamera::ImageCapturer::CameraProfile profile;
-        profile.invert_image  = get_parameter("invert_image").as_bool();
-        profile.gain          = 16.9807;
-        profile.exposure_time = std::chrono::milliseconds(get_parameter("exposure_time").as_int());
+        mode_ = CapturerMode{get_parameter("use_video").as_bool()};
 
-        capturer_ = std::make_unique<hikcamera::ImageCapturer>(profile);
+        if (mode_ == CapturerMode::VideoMode) {
+            RCLCPP_INFO(this->get_logger(), "Video mode is on.");
 
+            video_capture_ =
+                std::make_unique<cv::VideoCapture>(get_parameter("video_path").as_string());
+
+            if (!video_capture_->isOpened()) {
+                RCLCPP_ERROR(this->get_logger(), "Video file not found.");
+            }
+        } else {
+            RCLCPP_INFO(get_logger(), "Camera mode is on.");
+
+            hikcamera::ImageCapturer::CameraProfile profile;
+            profile.invert_image = get_parameter("invert_image").as_bool();
+            profile.gain         = 16.9807;
+            profile.exposure_time =
+                std::chrono::milliseconds(get_parameter("exposure_time").as_int());
+
+            camera_capturer_ = std::make_unique<hikcamera::ImageCapturer>(profile);
+        }
         RCLCPP_INFO(this->get_logger(), "Capturer initialized");
     }
 
@@ -35,13 +51,21 @@ public:
         if (thread_.joinable()) {
             thread_.join();
         }
+        video_capture_->release();
     }
 
     void update() override {
         if (*update_count_ == 0) {
             thread_ = std::thread([this] {
                 while (rclcpp::ok()) {
-                    auto img = capturer_->read();
+                    cv::Mat img;
+
+                    if (mode_ == CapturerMode::CameraMode) {
+                        img = camera_capturer_->read();
+                    } else if (video_capture_->isOpened()) {
+                        video_capture_->read(img);
+                    }
+
                     if (!img.empty()) {
                         buffer[!buffer_index_.load()] = img;
                         buffer_index_.store(!buffer_index_.load());
@@ -58,12 +82,15 @@ private:
     std::atomic<bool> buffer_index_{false};
     std::thread thread_;
 
-    std::unique_ptr<hikcamera::ImageCapturer> capturer_;
+    std::unique_ptr<hikcamera::ImageCapturer> camera_capturer_;
+    std::unique_ptr<cv::VideoCapture> video_capture_;
 
     cv::Mat buffer[2];
 
     InputInterface<size_t> update_count_;
     OutputInterface<cv::Mat> img_;
+
+    enum class CapturerMode { CameraMode = false, VideoMode = true } mode_;
 };
 } // namespace rmcs_auto_aim
 
