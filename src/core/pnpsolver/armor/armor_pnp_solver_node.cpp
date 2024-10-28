f7a7040f17d34ed1e9f2c35b382257cc47c65a97
7emotions <paradise_c@qq.com>
2024-10-28 12:32:42 +0000

    Feat: add armor pnp solver

--- src/core/pnpsolver/armor/armor_pnp_solver_node.cpp
+++ src/core/pnpsolver/armor/armor_pnp_solver_node.cpp
@@ -1,5 +1,14 @@
+#include <cstddef>
+#include <rmcs_description/tf_description.hpp>
+#include <vector>
+
 #include <rclcpp/node.hpp>
 #include <rmcs_executor/component.hpp>
+
+#include "core/frame.hpp"
+#include "core/identifier/armor/armor.hpp"
+#include "core/pnpsolver/armor/armor3d.hpp"
+#include "core/pnpsolver/armor/armor_pnp_solver.hpp"
 
 namespace rmcs_auto_aim {
 class ArmorPnpSolverNode
@@ -9,11 +18,48 @@ public:
     explicit ArmorPnpSolverNode()
         : rclcpp::Node(
               get_component_name(),
-              rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)) {}
+              rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)) {
+        register_input("/auto_aim/armor_plates", frame_input_);
+        register_input("/tf", tf_);
 
-    void update() override {}
+        register_output("/auto_aim/armor_plates_3d", frame_output_);
+
+        fx_ = declare_parameter<double>("fx");
+        fy_ = declare_parameter<double>("fy");
+        cx_ = declare_parameter<double>("cx");
+        cy_ = declare_parameter<double>("cy");
+        k1_ = declare_parameter<double>("k1");
+        k2_ = declare_parameter<double>("k2");
+        k3_ = declare_parameter<double>("k3");
+
+        RCLCPP_INFO(get_logger(), "Pnp Solver Node Initialized");
+    }
+
+    void update() override {
+        if (last_frame_id_ != frame_input_->frame_id_) {
+            frame_output_->frame_id_ = frame_input_->frame_id_;
+            auto result              = ArmorPnPSolver::SolveAll(
+                frame_input_->data_, *tf_, fx_, fy_, cx_, cy_, k1_, k2_, k3_);
+            if (!result.empty()) {
+                frame_output_->data_     = result;
+                frame_output_->frame_id_ = frame_input_->frame_id_;
+                if (publish_topic_) {
+                    // TODO: publish topic
+                }
+                last_frame_id_ = frame_input_->frame_id_;
+            }
+        }
+    }
 
 private:
+    double fx_, fy_, cx_, cy_, k1_, k2_, k3_;
+    bool publish_topic_{false};
+    size_t last_frame_id_{0};
+
+    InputInterface<rmcs_description::Tf> tf_;
+    InputInterface<struct rmcs_auto_aim::Frame<std::vector<ArmorPlate>>> frame_input_;
+
+    OutputInterface<struct rmcs_auto_aim::Frame<std::vector<ArmorPlate3d>>> frame_output_;
 };
 } // namespace rmcs_auto_aim
 
